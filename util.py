cccc1216ed52df55357d53e69fed751e2352b1b6
Ho Hai Thuyen <113648426+21dh114172@users.noreply.github.com>
2024-05-31 14:43:21 +0700

    add SaveBestModel

--- util.py
+++ util.py
@@ -3,7 +3,44 @@ import torch.nn.functional as F
 import torch
 import numpy as np
 
+def save_model(epochs, model, optimizer, criterion):
+    """
+    Function to save the trained model to disk.
+    """
+    print(f"Saving final model...")
+    torch.save({
+                'epoch': epochs,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': criterion,
+                }, 'outputs/final_model.pth')
 
+class SaveBestModel:
+    """
+    Class to save the best model while training. If the current epoch's 
+    validation loss is less than the previous least less, then save the
+    model state.
+    """
+    def __init__(
+        self, best_valid_loss=float('inf')
+    ):
+        self.best_valid_loss = best_valid_loss
+        
+    def __call__(
+        self, current_valid_loss, 
+        epoch, model, optimizer, criterion
+    ):
+        if current_valid_loss < self.best_valid_loss:
+            self.best_valid_loss = current_valid_loss
+            print(f"\nBest validation loss: {self.best_valid_loss}")
+            print(f"\nSaving best model for epoch: {epoch+1}\n")
+            torch.save({
+                'epoch': epoch+1,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': criterion,
+                }, 'outputs/best_model.pth')
+            
 def sigmoid_rampup(current, rampup_length):
     current = np.clip(current, 0.0, rampup_length)
     phase = 1.0 - current / rampup_length
