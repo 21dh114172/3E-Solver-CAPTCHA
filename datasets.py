96227c8862162267a47b5bca08076195aa6c1c40
Ho Hai Thuyen <113648426+21dh114172@users.noreply.github.com>
2024-06-13 17:48:36 +0700

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -90,8 +90,6 @@ def get_dataloader(filenames, label_dict, args, train, label, loader_len=None):
 
     img_buffer = np.zeros((len(filenames), TARGET_HEIGHT, TARGET_WIDTH, 3), dtype=np.uint8)
     text_buffer = []
-    print(label_dict)
-    sys.exit(1)
     for i, filename in enumerate(filenames):
         captcha_image = Image.open(filename).resize((TARGET_WIDTH, TARGET_HEIGHT), Image.ANTIALIAS)
         if captcha_image.mode != 'RGB':
@@ -104,7 +102,12 @@ def get_dataloader(filenames, label_dict, args, train, label, loader_len=None):
                 text = label_dict[filename.split("/")[-1]]
         else:
             #text = filename.split("/")[-1].split(".")[0] # get label from file name
-            text = label_dict[filename.split("/")[-1]]
+            try:
+                text = label_dict[filename.split("/")[-1]]
+            except:
+                print(label_dict)
+                print(filename.split("/")[-1])
+                sys.exit(1)
             
 
         if label:
